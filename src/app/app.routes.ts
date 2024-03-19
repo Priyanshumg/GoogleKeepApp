931146fbedf8e2d5e66a075da5e478ae1b5d6122
Priyanshumg <ginkalpriyanshu@gmail.com>
2024-03-19 16:30:20 +0530

    [Priyanshu] Add: Added Default Navigation to the Login Component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { LoginPageComponent } from './component/login-page/login-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+      path: '', redirectTo: 'login', pathMatch: 'full'
+    },
+    {
+      path: 'login',
+      component:LoginPageComponent
+    },
+
+];
