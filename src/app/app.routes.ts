c5dc5b3c3370070608b3ebf6113f8832825df1dd
Priyanshumg <ginkalpriyanshu@gmail.com>
2024-03-19 21:51:50 +0530

    [Priyanshu] Add: Added parentchild relationship to the route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginPageComponent } from './component/login-page/login-page.component';
+import { GoogleAppComponentComponent } from './component/google-app-component/google-app-component.component';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
 
 export const routes: Routes = [
     {
@@ -9,5 +11,14 @@ export const routes: Routes = [
       path: 'login',
       component:LoginPageComponent
     },
-
+    {
+      // Parent Component
+      path: '',
+      component:GoogleAppComponentComponent,
+      children:[{
+        // Child Components
+        path:'dashboard',
+        component:DashboardComponent
+      }]
+    }
 ];
