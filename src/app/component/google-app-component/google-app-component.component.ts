877bea5e19207f3ffe231550ec7728a4a2d2f605
Priyanshumg <ginkalpriyanshu@gmail.com>
2024-03-19 22:50:12 +0530

    [Priyanshu] Add: Added sidenav component to the Google-app-component.html

--- src/app/component/google-app-component/google-app-component.component.ts
+++ src/app/component/google-app-component/google-app-component.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { DashboardComponent } from '../dashboard/dashboard.component';
+import { SidenavComponent } from '../sidenav/sidenav.component';
 
 @Component({
   selector: 'app-google-app-component',
   standalone: true,
-  imports: [DashboardComponent],
+  imports: [DashboardComponent, SidenavComponent],
   templateUrl: './google-app-component.component.html',
   styleUrl: './google-app-component.component.scss'
 })
