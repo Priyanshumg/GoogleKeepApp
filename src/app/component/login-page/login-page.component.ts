0acbfdc1035fc92681baeb98d129885cf9181aa2
Priyanshumg <ginkalpriyanshu@gmail.com>
2024-03-19 16:31:16 +0530

    [Priyanshu] Add: Added component and template for the login

---
+++ src/app/component/login-page/login-page.component.ts
@@ -0,0 +1,40 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+@Component({
+  selector: 'app-login-page',
+  standalone: true,
+  imports: [FormsModule, HttpClientModule],
+  templateUrl: './login-page.component.html',
+  styleUrl: './login-page.component.scss'
+})
+export class LoginPageComponent {
+  loginobj:login;
+  constructor(private http: HttpClient){
+    this.loginobj = new login;
+  }
+
+  OnNext(){
+    debugger;
+    this.http.post("https://localhost:44347/api/User/Login",this.loginobj).subscribe((response:any) => {
+      if (response.result){
+        alert("Login Successfull")
+      }
+      else{
+        alert(response.message)
+      }
+    })
+  }
+}
+
+export class login{
+  user_Email: string;
+  user_Passwords: string;
+
+  constructor(){
+    this.user_Email = '';
+    this.user_Passwords = '';
+  }
+}
