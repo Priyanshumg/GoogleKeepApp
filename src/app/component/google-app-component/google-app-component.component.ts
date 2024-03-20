f18e89fa365399d9572a722ce382e1563d9bd543
Priyanshumg <ginkalpriyanshu@gmail.com>
2024-03-20 13:30:00 +0530

    [Priyanshu] Add

--- src/app/component/google-app-component/google-app-component.component.ts
+++ src/app/component/google-app-component/google-app-component.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { DashboardComponent } from '../dashboard/dashboard.component';
 import { SidenavComponent } from '../sidenav/sidenav.component';
+import { AddNotesComponent } from '../add-notes/add-notes.component';
 
 @Component({
   selector: 'app-google-app-component',
   standalone: true,
-  imports: [DashboardComponent, SidenavComponent],
+  imports: [DashboardComponent, SidenavComponent, AddNotesComponent],
   templateUrl: './google-app-component.component.html',
   styleUrl: './google-app-component.component.scss'
 })
