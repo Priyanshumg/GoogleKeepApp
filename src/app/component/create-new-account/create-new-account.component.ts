5d476e5ea5c07be25bda66ba4f741686334e8912
Priyanshumg <ginkalpriyanshu@gmail.com>
2024-03-20 12:39:12 +0530

    [Priyanshu] Add: Added new component to the app/component, CreateNewAccount

---
+++ src/app/component/create-new-account/create-new-account.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-new-account',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-new-account.component.html',
+  styleUrl: './create-new-account.component.scss'
+})
+export class CreateNewAccountComponent {
+
+}
