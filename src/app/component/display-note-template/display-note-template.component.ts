ecc5ae5839f01cf4fd5fc89c24a1abbc8986a8d2
Priyanshumg <ginkalpriyanshu@gmail.com>
2024-03-20 16:36:52 +0530

    [Priyanshu] Add: Added displayNote template

---
+++ src/app/component/display-note-template/display-note-template.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-display-note-template',
+  standalone: true,
+  imports: [],
+  templateUrl: './display-note-template.component.html',
+  styleUrl: './display-note-template.component.scss'
+})
+export class DisplayNoteTemplateComponent {
+
+}
