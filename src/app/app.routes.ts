6bfc1a90bd6efb4b39e4e59896cdc269910844eb
Priyanshumg <ginkalpriyanshu@gmail.com>
2024-03-20 16:33:52 +0530

    [Priyanshu] Add: Added routes to the app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { LoginPageComponent } from './component/login-page/login-page.component'
 import { GoogleAppComponentComponent } from './component/google-app-component/google-app-component.component';
 import { DashboardComponent } from './component/dashboard/dashboard.component';
 import { CreateNewAccountComponent } from './component/create-new-account/create-new-account.component';
+import { DisplayNoteTemplateComponent } from './component/display-note-template/display-note-template.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' },
@@ -15,7 +16,8 @@ export const routes: Routes = [
             { path: 'top', component: DashboardComponent }
         ]
     },
-    { path: 'create', component: CreateNewAccountComponent }
+    { path: 'create', component: CreateNewAccountComponent },
+    { path: 'display', component: DisplayNoteTemplateComponent}
 ];
 
 @NgModule({
