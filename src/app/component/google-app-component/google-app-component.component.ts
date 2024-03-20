14ccd19897ced59f6f09cc902ad72f245d88932b
Priyanshumg <ginkalpriyanshu@gmail.com>
2024-03-20 16:34:56 +0530

    [Priyanshu] Mod: Modified changes in GoogleKeepAppComponent, added displayNote

--- src/app/component/google-app-component/google-app-component.component.ts
+++ src/app/component/google-app-component/google-app-component.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { DashboardComponent } from '../dashboard/dashboard.component';
 import { SidenavComponent } from '../sidenav/sidenav.component';
 import { AddNotesComponent } from '../add-notes/add-notes.component';
+import { DisplayNoteTemplateComponent } from '../display-note-template/display-note-template.component';
 
 @Component({
   selector: 'app-google-app-component',
   standalone: true,
-  imports: [DashboardComponent, SidenavComponent, AddNotesComponent],
+  imports: [
+    DashboardComponent,
+    SidenavComponent,
+    AddNotesComponent,
+    DisplayNoteTemplateComponent
+  ],
   templateUrl: './google-app-component.component.html',
   styleUrl: './google-app-component.component.scss'
 })
